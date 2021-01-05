28dc5a873a5527c0e5ec3ea077e6e7ea55f53945
ebenezer <e.kankam95@hotmail.com>
2021-01-05 12:05:07 -0600

    added a function to generate unique ids

--- module-4-assets/js/app.js
+++ module-4-assets/js/app.js
@@ -1,7 +1,7 @@
 const buttonEl = document.querySelector('#save-task');
 const taskToDoEl = document.querySelector('#tasks-to-do');
 const formEl = document.querySelector('#task-form');
-
+let taskIdCounter = 0;
 
 
 const taskFormHandler = (e) => {
@@ -30,6 +30,8 @@ const createTaskEl = (taskDataObj) => {
     // create a list item
     const listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
+    // add task id as a  custom attribute
+    listItemEl.setAttribute('data-task-id', taskIdCounter);
     // create a div to hold task info and add to list item
     const taskInfoEl = document.createElement('div');
     // give it a classname
@@ -37,13 +39,53 @@ const createTaskEl = (taskDataObj) => {
     // add HTML content to div
     taskInfoEl.innerHTML = `
         <h3 class='task-name'>${taskDataObj.name}</h3>
-        <span class='task-type'>${taskDataObj.name}</span>
+        <span class='task-type'>${taskDataObj.type}</span>
     `;
     // add div to li
     listItemEl.appendChild(taskInfoEl);
+
+    const taskActionsEl = createTaskActions(taskIdCounter);
+    listItemEl.appendChild(taskActionsEl);
     // add entire list item to list
     taskToDoEl.appendChild(listItemEl);
+    // increase task counter for next unique id
+    taskIdCounter++;
 }
 
+const createTaskActions = (taskId) => {
+    const actionContainerEl = document.createElement('div');
+    actionContainerEl.className = 'task-actions';
+    // create edit button
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.className = 'btn edit-btn';
+    editButton.setAttribute('data-task-id', taskId);
+    actionContainerEl.appendChild(editButton);
+
+    // create delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Edit';
+    deleteButton.className = 'btn delete-btn';
+    deleteButton.setAttribute('data-task-id', taskId);
+    actionContainerEl.appendChild(deleteButton);
+
+    // create dropdown button
+    const statusSelectEl = document.createElement('select');
+    statusSelectEl.className = 'select-status';
+    statusSelectEl.setAttribute('name', 'status-change');
+    statusSelectEl.setAttribute('data-task-id', taskId);
+    actionContainerEl.appendChild(statusSelectEl);
+
+    const statusChoices = ['To Do', 'In Progress', 'Completed'];
+    for (let i = 0; i < statusChoices.length; i++) {
+        // create option element
+        const statusOption = document.createElement('option');
+        statusOption.textContent = statusChoices[i];
+        statusOption.setAttribute('value', statusChoices[i]);
+        // append to select
+        statusSelectEl.appendChild(statusOption);
+    }
+    return actionContainerEl;
+}
 
 formEl.addEventListener('submit', taskFormHandler);
